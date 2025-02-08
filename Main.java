7fc47ad3176b2913952f4d6816f4b81360c0b51a
dinhvinhspkt <dinhvinhspkt@gmail.com>
2025-02-08 17:10:07 +0900

    commit- Branch 2

--- Main.java
+++ Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Test main");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number 1");
+        int a = sc.nextInt();
+        System.out.println("Enter number 2");
+        int b = sc.nextInt();
+        int sum = tinhtong(a,b);
+        System.out.println("sum = " + sum);
+    }
+    private static int tinhtong(int a, int b){
+        return a+b;
     }
 }
